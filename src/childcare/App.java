428c55bba35205664b7fb61f26d2c9ab1dc544ec
zolinz <zoltankovacs_nz@yahoo.co.nz>
2017-06-02 12:17:55 +1000

    simple childcare attribute

--- src/childcare/App.java
+++ src/childcare/App.java
@@ -7,15 +7,20 @@ public class App {
     public static void main(String args[]) {
 
         ChildCare cc = new ChildCare();
-        cc.initializeChildren();
-        cc.getChildren().add(new Child("sebi"));
-        cc.getChildren().add(new Child("mari"));
 
 
-        for (Child c : cc.getChildren()){
+        System.out.println(cc.getCcName());
+
+        
+        //cc.initializeChildren();
+        //cc.getChildren().add(new Child("sebi"));
+        //cc.getChildren().add(new Child("mari"));
+
+
+      /*  for (Child c : cc.getChildren()){
 
             System.out.println(c.getName());
-        }
+        }*/
 
 
 
--- src/childcare/ChildCare.java
+++ src/childcare/ChildCare.java
@@ -7,16 +7,24 @@ public class ChildCare {
 
     private List<Child> children;
 
+    private String ccName;
+
 
     public ChildCare(){
 
+        ccName = "one tree";
        // children = new ArrayList<>();
     }
 
 
     public void initializeChildren(){
         //create a new arralist ready to add children
-        children = new java.util.ArrayList<Child>();
+       // children = new java.util.ArrayList<Child>();
+    }
+
+
+    public String getCcName(){
+        return ccName;
     }
 
 
